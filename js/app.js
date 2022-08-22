b96e1b039d887f27cd2d82c8d54db99cf3914ebd
sheikh-momin <momin.emon14@gmail.com>
2022-08-22 08:26:25 -0700

    add image

--- js/app.js
+++ js/app.js
@@ -6,47 +6,16 @@ function playerSelect(event){
     
     newItem.innerText = itemValue
     orderList.appendChild(newItem)
+  
   }
   else{
     alert('Player list is full already')
   }
+  
 }
 
-document.getElementById('calculate-btn').addEventListener('click', ()=>{
-const orderList =document.getElementById('order-list')
-  const playerExpenses = document.getElementById('player-expenses')
-  const perPlayerCostString =document.getElementById('per-player-cost')
-  const perPlayerCostValue = perPlayerCostString.value
-  const perPlayerCost = parseInt(perPlayerCostValue)
-  const calculateExpenses= perPlayerCost * orderList.children.length
-  if (perPlayerCostString.value!== ''){
-  playerExpenses.innerText= calculateExpenses
-  perPlayerCostString.value = ''
-  }
-  else {
-    alert('Enter Player Expenses!')
-  }
 
 
-
-document.getElementById('calculate-total-btn').addEventListener('click', ()=>{
-  
-  const totalCost = document.getElementById('total-cost')
-  const managerCostString =document.getElementById('manager-cost')
-  const managerCostValue = managerCostString.value
-  const managerCost = parseInt(managerCostValue)
-  const coachCostString = document.getElementById('coach-cost')
-  const coachCostValue = coachCostString.value
-  const coachCost = parseInt(coachCostValue)
-    if(managerCostString.value !== '' && coachCostString.value !==''){
-
-      totalCost.innerText =calculateExpenses +  managerCost + coachCost
-    }
-    else{
-      alert('Enter Manager and Coach cost properly!')
-    }
-  })
-})
 
 
 
